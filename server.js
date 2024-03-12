b9f0b690cfac855699a058614cee9c934cabf8c8
John Paul Damasco <damasco.johnpaul@gmail.com>
2024-03-13 02:55:45 +0800

    test1

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 4000;
 
 server.use(middlewares);
 server.use("", router);
-server.listen(process.env.PORT || 5000, () => {
+server.listen(port, () => {
   console.log("JSON Server is running");
 });
